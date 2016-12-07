7796b69e8aead867ea3cfc4b49422c41a19ab083
losvenenos2017 <robinperez2490@gmail.com>
2016-12-06 21:20:02 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,53 +1,101 @@
-<?php
-
-    include_once('db/utilidades_sql.php');
-    include_once ('php/utilidades.php');
-?>
-<!--    -->
-          <?php include_once('php/header.inc'); ?>
-
-          <div class="contenido">
-            <?php include_once('php/slider.inc'); ?>
-
-              <div id="centro">
-                  <h1>QUIENES SOMOS</h1>
-                    <p> Barber shop, es una empresa legalmente constituida desde el año 2016, la cual nació con la finalidad de satisfacer las necesidades de hombres y mujeres a partir de los 6 meses.
-
-                    Tras estudiar, desarrollar practicas al interior de la empresa y capacitarnos continuamente en asesoría de imagen y conocimiento de productos para el cuidado del cabello y la piel, nos dedicamos a realzar la imagen de nuestros consumidores, para que brillen con luz propia. Ofrecemos a nuestros clientes un servicio que se caracteriza por su excelencia en la prestación de servicios, ética empresarial, profesionalismo y un enfoque creativo para resaltar la imagen de nuestros usuarios. </p>
-
-                    <a href="nosotros.php"> <strong> Leer Mas... </strong> </a>
-              </div>
-
-              <div id="fl-derecho">
-                <h1>OFICIO</h1>
-                <p>Queremos ayudar a relanzar uno de los oficios más valorados para el público masculino y femenino. Te encontrarás en un entorno confortable y con toda la esencia de las barberías más tradicionales americanas.
-                </p>
-                
-              </div>
-
-              <div id="fl-centro">
-                <h1>SERVICIOS</h1>
-                <p> Ofrecemos cortes de cabello exclusivos para niños, adolescentes y caballeros. Te cortamos el pelo a máquina o a tijera, te arreglamos la barba o te afeitamos con un ritual de toallas que te sorprenderá.
-                </p>
-                
-              </div>
-
-              <div id="fl-izquierda">
-                <h1>RESERVAS</h1>
-                <p> Ya puedes realizar tu reserva mediante internet, además de por teléfono. Entra con tus datos personales y reserva ahora en cualquiera de nuestras barberías.
-                </p>
-                
-              </div> 
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="refresh" content="15">
+        <title>LatinosBarberShop</title>
+        <link rel="stylesheet" href="css/style.css" type="text/css" />
+        <script src="js/slider.js" type="text/javascript"></script>
+    </head>
+    
+    <body>
+        <div id="imagen-derecha">
+                <img src="img/derecho2.png">
+        </div>
+        
+        <header>
+            <nav> 
+                <ul>
+                    <li> <a href=""> Inicio</a></li>
+                    <li> <a href=""> Nosotros</a></li>
+                    <li> <a href=""> Galeria</a></li>
+                    <li> <a href=""> Contactenos</a></li>
+                    <li> <a href=""> Login</a></li>
+                    <li> <a href="" id="Registro"> Registro</a></li>
+                </ul>
+            </nav>
+        </header>    
+        <!--    -->
+        <div id='slider'>
+            <div class="slider-inner">
+                <ul>
+                    <li> <a class="ns-img" href="img/slide1.jpg"></a> </li>
+                    <li> <a class="ns-img" href="img/slide2.jpg"></a> </li>
+                    <li> <a class="ns-img" href="img/slide4.jpg"></a> </li>
+                    <li> <a class="ns-img" href="img/slide5.jpg"></a> </li>
+                </ul>    
             </div>
-        </section>
+        </div>
+        
+        <div id="columna">
+            <div id="columna1"> 
+                <h1><center>OFICIO</center></h1>
+                    <p>Queremos ayudar a relanzar uno de los oficios más valorados para el público masculino y femenino.
+                        Te encontrarás en un entorno confortable y con toda la esencia de las barberías más tradicionales
+                        americanas.
+                    </p>
+            </div>
 
-        <section class="bloque3">
-          <img id="derecho" src="img/derecho.png">
-        </section>
+            <div id="columna2"> 
+                <h1><center>SOBRE NOSOTROS</center></h1>
+                    <p>Barberos y estilistas especializados que cuidan de tu look. 
+                        Sabemos lo que te quedará mejor, como cortarte el pelo y arreglarte la barba.
+                        Cuidamos mucho el detalle y el trato con el cliente.
+                    </p>
+            </div>
 
-      </div>
-      
-      <?php include_once ('php/footer.inc');  ?> 
+            <div id="columna3"> 
+               <h1><center>SERVICIOS</center></h1>
+                    <p>Ofrecemos cortes de cabello exclusivos para niños, adolescentes y caballeros.
+                        Te cortamos el pelo a máquina o a tijera, te arreglamos la barba o te afeitamos
+                        con un ritual de toallas que te sorprenderá.
+                    </p>
+            </div>
+
+            <div id="columna4">
+                <h1><center>RESERVAS</center></h1>
+                    <p>Ya puedes realizar tu reserva mediante internet, además de por teléfono.
+                    Entra con tus datos personales y reserva ahora en cualquiera de nuestras barberías.
+                    </p>
+            </div>
+        </div> 
+
+        <h1 style=" text-align: center;"> Disfrute de Nuestros Videos</h1>
+        
+        <div class="video-responsive">
+            <iframe src="//www.youtube.com/embed/srAp9pzvRoE" frameborder="0" allowfullscreen width="560" height="315"></iframe>
+        </div>
+        
+        <nav> 
+                <ul>
+                    <li> <a href=""> Inicio</a></li>
+                    <li> <a href=""> Nosotros</a></li>
+                    <li> <a href=""> Galeria</a></li>
+                    <li> <a href=""> Contactenos</a></li>
+                    <li> <a href=""> Login</a></li>
+                    <li> <a href="" id="Registro"> Registro</a></li>
+                </ul>
+            </nav>
+        
+        
+        <footer>
+            <p>Copyright &copy; LatinosBarberShop 2016</p>
+        </footer>
 
     </body>
 </html>
